0ef2f4b69f95ee920b107fae837351e85cf0b76a
Sergio Tapia-Fikes <stapiafikes@gmail.com>
2019-09-05 22:12:40 -0500

    add shuffle at start of game and after all cards match  9/5

--- memory.js
+++ memory.js
@@ -7,10 +7,15 @@ const hearts7 = document.getElementById('hearts7');
 const cards = document.querySelectorAll('.front-card'); 
 const backCards = document.querySelectorAll('.back-card'); 
 const cardContainer = document.querySelectorAll('.front-back'); 
-var rankArray =[]; 
-var tempArray =[]; 
+let rankArray =[]; 
+let tempArray =[];
 let click = 0; 
+let shuffleArray = [0, 1, 2, 3, 4, 5]; 
+let cardContainerArray = [];
 
+shuffleCards();
+
+cardContainer.forEach(cardContainer =>  cardContainerArray.push(cardContainer)); 
 cards.forEach((card,index) => card.setAttribute("data-index", index)); 
 cardContainer.forEach(container => container.addEventListener("click", displayCard)); 
 cards.forEach(function(card){
@@ -38,6 +43,8 @@ function displayCard() {
 
     if (click == 2 && tempArray[1].elementId != tempArray[0].elementId) {
         cardContainer[index].firstElementChild.style.display ="block"; 
+        
+
     } else if (click ==2 && tempArray[1].elementId == tempArray[0].elementId) {
         click -= 1; 
         tempArray.splice(1,1); 
@@ -67,45 +74,44 @@ function displayCard() {
         break; 
         }
     },1000);
-   
+
+  
 }
 
 
-let shuffleArray = [0, 1, 2, 3, 4, 5]; 
-
-let cardContainerArray = [];
-cardContainer.forEach(cardContainer =>  cardContainerArray.push(cardContainer)); 
-
-function shuffleCardIndex() {
-    if (cardContainerArray.every(x => x.getAttribute("data-matched") =="true") ) { 
+function shuffleCards() {
     let newArray = shuffle(shuffleArray); 
 
     for(let i =0; i< newArray.length; i++){
         cardContainer[i].style.order = newArray[i]; 
     }
-    cards.forEach(card => card.style.display ="none");
-    cardContainer.forEach(container => container.addEventListener("click", displayCard)); 
-    setTimeout(function() {
-        if (cardContainerArray.every(x => x.getAttribute("data-matched") =="true")) {
-            cardContainer.forEach(container => container.dataset.matched = "false");
-        }
-    }, 1500); 
-}
 
 }
-//below function very similar to approach to reversing an array in place with the task of swapping values. 
-/* fisher-yates-shuffle randomly swapping index values starting
-at end of array and working down to index 0 */
+
+function shuffleCardIndex() {
+    if (cardContainerArray.every(x => x.getAttribute("data-matched") =="true") ) { 
+        shuffleCards(); 
+        cards.forEach(card => card.style.display ="none");
+        cardContainer.forEach(container => container.addEventListener("click", displayCard)); 
+        setTimeout(function() {
+            if (cardContainerArray.every(x => x.getAttribute("data-matched") =="true")) {
+                cardContainer.forEach(container => container.dataset.matched = "false");
+            }
+        }, 1500); 
+    }
+
+}
+/*below function very similar to approach to reversing an array in place with the task of swapping values. 
+ fisher-yates-shuffle */
 
 function shuffle(array) {
     var i = array.length, // i assigned value of array.length 
-        j = 0, // j assigned value of 0 
-        temp; // var temp declared but uninitalized value a.k.a undefined value 
+        j,  
+        temp; // var temp & j declared but uninitalized values a.k.a undefined values 
 
     while (i--) {
 
         j = Math.floor(Math.random() * (i+1)); 
-
         /* swap randomly chosen value with current value work through array 
         from end to start*/
         temp = array[i];
@@ -118,12 +124,21 @@ function shuffle(array) {
 }
 
 
-/*
- add play new game feature at end of game which can shuffle the cards
-and start over. would be really cool if cards actually shuffle on the screen*/
 
-// -----IMPORTANT------ read below 
-/*to shuffle I can use flexBox Order on each Image.. 
+ 
+/*
+--- Below are notes for self--- 
+
+-- add play new game feature at end of game which can shuffle the cards
+and start over. would be really cool if cards actually shuffle on the screen however this 
+would require me to change away from flex-box order. and use a grid instead..and then adjust 
+
+-- need to figure out how to fix if someone clicks 3 cards very quickly that timeOut 
+runs out of time so it leaves cards up even without match because I splice values from 
+array.. maybe use loop and index and don't splice. Going to have to rework the code. 
+
+---More Ideas--- below mostly already implemented. 
+
 const choices = cards; // nodelist of all front-cards. convert to an array maybe? 
 
 maybe make the folliowing a method: Math.floor(Math.random() * 6);
@@ -133,9 +148,6 @@ double assign the same order... have to put logic in method to see if order is
 already assigned.. if assigned run the random method again 
 maybe something like 
 while (array.includes(order)) run method again
-
-create an array with values 1-6 and put these numbers in random order everytime method 
-is run.
 
 array.every could be used to see if every array[index value].data-matched == "true"
 if true then run something to show a game win message with inner HTML then setTimeout
